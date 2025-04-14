1818d91ed597074b0f018f3c38a1f35d19bc45de
mBalao9 <jessi.hane@gmail.com>
2025-04-14 10:50:41 -0300

    feat: add TaskItem component

---
+++ src/components/TaskItem.js
@@ -0,0 +1,17 @@
+const TaskItem = (props) => {
+    return (
+        <>
+            <h1>Tarefas</h1>
+            <h2>Usuário: {props.tarefa.usuario}</h2>
+            <h3>Descrição: {props.tarefa.description}</h3>
+            <h3>id: {props.tarefa.id}</h3>
+            <p>
+                {props.tarefa.isCompleted
+                    ? "Tarefa completa."
+                    : "Tarefa não completa."}
+            </p>
+        </>
+    );
+};
+
+export default TaskItem;

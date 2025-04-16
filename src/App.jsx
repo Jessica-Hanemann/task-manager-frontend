8e8b2d09d40dc6d79dcbe727936b5278b5d4074e
mBalao9 <jessi.hane@gmail.com>
2025-04-16 10:30:12 -0300

    feat: add taskItem component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import TaskItem from "./components/TaskItem";
+import Tasks from "./components/Tasks";
+
+const App = () => {
+    return (
+        <>
+            <Tasks />
+        </>
+    );
+};
+
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./Tasks.scss";
+
+const Tasks = () => {
+    const [tasks, setTasks] = useState([]);
+
+    const fetchTasks = async () => {
+        try {
+            //data é onde estão as tarefas
+            const { data } = await axios.get("http://localhost:8000/tasks");
+            setTasks(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    return (
+        <div className="tasks-container">
+            <h2>Minhas tarefas</h2>
+            <div className="last-tasks">
+                <h3>Últimas tarefas</h3>
+                <div className="tasks-list">
+                    {tasks
+                        .filter((task) => task.isCompleted === false)
+                        .map((lastTask) => (
+                            <p>{lastTask.description}</p>
+                        ))}
+                </div>
+            </div>
+            <div className="completed-tasks">
+                <h3>Tarefas concluídas</h3>
+                <div className="tasks-list">
+                    {tasks
+                        .filter((task) => task.isCompleted === true)
+                        .map((completedTask) => (
+                            <p>{completedTask.description}</p>
+                        ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Tasks;

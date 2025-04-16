93619ef033a788b0874da30d39b50bafc4cc8899
mBalao9 <jessi.hane@gmail.com>
2025-04-16 11:29:52 -0300

    feat: update taskItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import "./App.css";
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-// import TaskItem from "./components/TaskItem";
 import Tasks from "./components/Tasks";
 
 const App = () => {
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,15 +1,35 @@
+import "./TaskItem.scss";
+import { AiFillDelete } from "react-icons/ai";
 const TaskItem = (props) => {
     return (
         <>
-            <h1>Tarefas</h1>
-            <h2>Usuário: {props.tarefa.usuario}</h2>
-            <h3>Descrição: {props.tarefa.description}</h3>
-            <h3>id: {props.tarefa.id}</h3>
-            <p>
-                {props.tarefa.isCompleted
-                    ? "Tarefa completa."
-                    : "Tarefa não completa."}
-            </p>
+            <div className="task-item-container">
+                <div className="task-description">
+                    <label
+                        className={
+                            props.tarefa.isCompleted
+                                ? "checkbox-container-completed"
+                                : "checkbox-container"
+                        }
+                    >
+                        {props.tarefa.description}
+                        <input
+                            type="checkbox"
+                            defaultChecked={props.tarefa.isCompleted}
+                        />
+                        <span
+                            className={
+                                props.tarefa.isCompleted
+                                    ? "checkmark completed"
+                                    : "checkmark"
+                            }
+                        ></span>
+                    </label>
+                </div>
+                <div className="delete">
+                    <AiFillDelete size={18} color="#f97474" />
+                </div>
+            </div>
         </>
     );
 };

9847b3098431db418f6ceb90a0d0638789c60648
mBalao9 <jessi.hane@gmail.com>
2025-06-04 13:46:55 -0300

    feat: add navigate to login from home

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import CustomButton from "./CustomButton";
 import logo from "../assets/images/logo-taskmanager.png";
 import "./Sidebar.scss";
 
 const Sidebar = () => {
+    const navigate = useNavigate();
+    const handleSignOutClick = () => {
+        navigate("/login");
+    };
     return (
         <div className="sidebar-container">
             <div className="logo">
                 <img src={logo} alt="FSC" />
             </div>
             <div className="sign-out">
-                <CustomButton>Sair</CustomButton>
+                <CustomButton onClick={handleSignOutClick}>Sair</CustomButton>
             </div>
         </div>
     );

dae4bdc5b1ac328357ab93f04a4059e6fadff518
mBalao9 <jessi.hane@gmail.com>
2025-06-04 13:33:27 -0300

    feat: add login component

---
+++ src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import logo from "../assets/images/logo-2.png";
+import CustomButton from "../components/CustomButton";
+import "./logo.scss";
+
+const Login = () => {
+    return (
+        <div className="login-container">
+            <img src={logo} alt="FSC" />
+            <div className="button-container">
+                <CustomButton>Entrar</CustomButton>
+            </div>
+        </div>
+    );
+};
+export default Login;

cbe4ce39c00603c440f255718576f6777798d5a3
mBalao9 <jessi.hane@gmail.com>
2025-06-04 13:44:43 -0300

    feat: navigate to home page in login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/images/logo-2.png";
 import CustomButton from "../components/CustomButton";
 import "./Login.scss";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const handleSignInClick = () => {
+        navigate("/");
+    };
+
     return (
         <div className="login-container">
             <img src={logo} alt="FSC" />
             <div className="button-container">
-                <CustomButton>Entrar</CustomButton>
+                <CustomButton onClick={handleSignInClick}>Entrar</CustomButton>
             </div>
         </div>
     );
